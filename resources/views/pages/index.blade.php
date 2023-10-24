3c04c7a555ec188a55bb42c254c54ade5bce902e
kvanags <kvanags@firestormmedia.tv>
2023-10-24 07:11:53 +0100

    fix view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -18,24 +18,24 @@
                         </div>
                     </div>
                     <div class="flex flex-row">
-                        <div class="hidden flex flex-row" id="expanded-cat-{{$cat['id']}}">
+                        <div onclick="hideCategory({{ $cat['id'] }})" class="hidden flex flex-row" id="expanded-cat-{{$cat['id']}}">
                             <div class="flex flex-col justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-rose-600 w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </div>
                             <div class="flex flex-col justify-center">
-                                <button class="px-4" onclick="hideCategory({{ $cat['id'] }})">Hide All</button>
+                                <button class="px-4">Hide All</button>
                             </div>
                         </div>
-                        <div class="flex flex-row"  id="collapsed-cat-{{$cat['id']}}">
+                        <div onclick="expandCategory({{ $cat['id'] }})" class="flex flex-row"  id="collapsed-cat-{{$cat['id']}}">
                             <div class="flex flex-col justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-slate-600 w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
                             </div>
                             <div class="flex flex-col justify-center">
-                                <button class="px-4" onclick="expandCategory({{ $cat['id'] }})">Show All</button>
+                                <button class="px-4">Show All</button>
                             </div>
                         </div>
                     </div>
