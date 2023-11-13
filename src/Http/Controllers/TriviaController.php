d35e62daf48d8306d94a2bd88e95d0cbc7a085f3
kvanags <kvanags@firestormmedia.tv>
2023-11-13 12:39:00 +0000

    there is not enough multilanguage questions

--- src/Http/Controllers/TriviaController.php
+++ src/Http/Controllers/TriviaController.php
@@ -916,7 +916,6 @@ class TriviaController
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setup($ch, CURLOPT_TIMEOUT, 30);
 
             $response = curl_exec($ch);
             curl_close($ch);
