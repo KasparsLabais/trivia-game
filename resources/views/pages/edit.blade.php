b5d5a9c15373b0fe2e60235e42aa06f779402bf8
kvanags <kvanags@firestormmedia.tv>
2023-12-17 15:27:10 +0000

    initial moving mangement paghe to vue

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,5 +1,8 @@
 @extends('game-api::layout')
 @section('body')
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
     <div class="flex flex-row justify-center" id="question-editor">
         <div class="flex flex-col mt-2 px-2  w-11/12">
             <div>
@@ -65,6 +68,80 @@
                     </form>
 
                     <div class="flex flex-col px-4 mt-4">
+                        <h2 class="raleway">Questions:</h2>
+                        <div v-for="question in sortedQuestions" class="flex flex-col bg-slate-200 shadow rounded mt-4">
+
+                            <div class="flex flex-row bg-slate-300  py-2 px-2">
+                                <div class="rounded bg-lime-600 px-1 cursor-pointer" v-on:click="moveQuestionOrderNrUp(question.id)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke-slate-200 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                                    </svg>
+                                </div>
+                                <div class="rounded bg-rose-600 px-1 cursor-pointer" v-on:click="moveQuestionOrderNrDown(question.id)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke-slate-200 w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </div>
+                                <span>
+                                    Order Nr: [[ question.order_nr ]]
+                                 </span>
+                                |
+                                <span class="text-xl px-4">[[ question.question ]]</span>
+                            </div>
+                            <div class="flex flex-row py-4">
+                                <template v-for="answer in question.answers">
+
+                                    <div v-if="answer.is_correct" class="border-lime-500 bg-lime-500 text-slate-100 font-semibold border-2 px-4 py-2 rounded shadow mx-1">
+                                        [[ answer.answer ]]
+                                    </div>
+                                    <div v-else class="border-slate-500 bg-slate-500 text-slate-100 font-semibold border-2 px-4 py-2 rounded shadow mx-1">
+                                        [[ answer.answer ]]
+                                    </div>
+                                    <template v-if="question.question_type == 'text_input'">
+                                        <form method="POST" v-bind:["action"]="'/admin/trv/answer-image/' + answer.id" enctype="multipart/form-data">
+                                            <div class="flex flex-row">
+                                                {{ csrf_field() }}
+                                                <div class="flex flex-col">
+
+                                                    <div v-if="answer.file_url_type != '' " class="flex flex-col">
+                                                        <a class="font-semibold text-cyan-700" v-bind:["href"]="answer.file_url" target="_blank">Current File</a>
+                                                    </div>
+
+                                                    <label>Upload Image For Correct answer: </label>
+                                                    <input type="file" name="answer-image" id="answer-image">
+                                                </div>
+                                                <button class="py-2 px-2 shadow-md bg-lime-600 text-left text-slate-100 text-lg font-semibold mb-2 rounded" type="submit">Upload Image</button>
+                                            </div>
+                                        </form>
+                                    </template>
+                                </template>
+                            </div>
+
+                            <div class="flex flex-row bg-slate-300 py-2 px-2">
+                                <div class="flex flex-row">
+                                    <div class="flex flex-col px-2 py-1">
+                                        <label class="raleway font-semibold text-sm" >Answer:</label>
+                                        <input class="bg-slate-100 border border-zinc-400 shadow shadow-zinc-400 rounded" type="text">
+                                    </div>
+                                    <template v-if="question.question_type == 'options'">
+                                        <div class="flex flex-col px-2 py-1">
+                                            <label class="raleway font-semibold text-sm" >Is Correct:</label>
+                                            <select class="bg-slate-100 border border-zinc-400 shadow shadow-zinc-400 rounded" >
+                                                <option value="0">No</option>
+                                                <option value="1">Yes</option>
+                                            </select>
+                                        </div>
+                                    </template>
+                                </div>
+                                <div class="flex flex-col justify-center">
+                                    <button v-on:click="addAnswer(question.id)" class="py-2 px-2 shadow-md bg-cyan-500 text-slate-100 font-semibold">Add Answer</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
                         <h2 class="raleway">Questions:</h2>
                         <div id="question-holder">
                         @foreach($questions as $question)
@@ -167,19 +244,51 @@
 
     <script>
 
+
         let questionsList = {
             @foreach($questions as $question)
-            '{{ $question['id'] }}' :{ 'id' :{{ $question['id'] }}, 'order_nr':{{ $question['order_nr'] }}, 'question':'{{ $question['question'] }}', 'answers':[
-                @foreach($question->answers as $answer)
-                {
-                    'id': {{ $answer['id'] }},
-                    'answer': '{{ $answer['answer'] }}',
-                    'is_correct': {{ $answer['is_correct'] }}
-                },
-                @endforeach
-            ]},
-        @endforeach
+            '{{ $question['id'] }}' :{ 'id' :{{ $question['id'] }}, 'order_nr':{{ $question['order_nr'] }}, 'question':'{{ $question['question'] }}', 'question_type': '{{ $question['question_type']  }}', 'answers':[
+                        @foreach($question->answers as $answer)
+                    {
+                        'id': {{ $answer['id'] }},
+                        'answer': '{{ $answer['answer'] }}',
+                        'is_correct': {{ $answer['is_correct'] }},
+                        'file_url': '{{ $answer['file_url'] }}',
+                        'file_url_type': '{{ $answer['file_url_type'] }}'
+                    },
+                    @endforeach
+                ]},
+            @endforeach
         };
+
+
+        const { createApp } = Vue;
+        createApp({
+            data() {
+                return {
+                    questions: questionsList
+                }
+            },
+            delimiters: ['[[', ']]'],
+            method: {
+
+            },
+            computed: {
+                sortedQuestions() {
+                    return Object.values(this.questions).sort((a, b) => a.order_nr - b.order_nr);
+                }
+            },
+            mount() {
+                console.log('mounted');
+            },
+        }).mount('#question-editor');
+
+
+
+
+
+
+
 
         const addQuestion = () => {
             console.log('add question');
